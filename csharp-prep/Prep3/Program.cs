d0021eafa86769e0f621c5495fc249b513d7e8fa
SabrinaCloud <cha24006@byui.edu>
2024-09-20 10:23:01 +0800

    finished prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        while (playAgain.ToLower()=="yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int attempts=0; //Tracing gusses number
+            int guess = -1;
+            while (guess != magicNumber)
+            {   
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                 else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!  It took you {attempts} guesses.");
+                }
+            }
+        Console.Write("Do you want to play again? (yes or no): ");
+        playAgain = Console.ReadLine();
+        }
     }
 }
