97d9f1ec27ed91955c538901e35b380e04e06402
SabrinaCloud <cha24006@byui.edu>
2024-10-22 20:26:20 +0800

    finished W06 learning activity assignment

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("blue", 2);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("yellow", 3,5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Pink", 7);
+        shapes.Add(s3);
+
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
